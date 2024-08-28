dabda84b374b532cd166d3970b369c8638586922
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 00:16:53 +0300

    Created users table

--- database/migrations/2024_08_28_195802_create_users_table.php
+++ database/migrations/2024_08_28_195802_create_users_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('type', ['teacher', 'student', 'parent', 'admin']);
+            $table->date('date_of_birth');
+            $table->string('address')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->foreignId('parent_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->string('parent_name')->nullable();
+            $table->string('parent_email')->nullable();
             $table->timestamps();
         });
     }
