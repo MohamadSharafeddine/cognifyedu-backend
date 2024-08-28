8265a5fbf1e709c0ce4f1d1ebeea3364c4f29ab9
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 00:06:42 +0300

    Created ProfileComment model and its associated files

---
+++ app/Http/Controllers/ProfileCommentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProfileComment;
+use App\Http\Requests\StoreProfileCommentRequest;
+use App\Http\Requests\UpdateProfileCommentRequest;
+
+class ProfileCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProfileCommentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProfileComment $profileComment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProfileComment $profileComment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProfileCommentRequest $request, ProfileComment $profileComment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProfileComment $profileComment)
+    {
+        //
+    }
+}
