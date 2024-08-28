12ffc167d874e75e96992e919ca9e3aa6021e5c1
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-28 23:46:33 +0300

    Created JoinRequest model and its associated files

---
+++ app/Http/Controllers/JoinRequestController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JoinRequest;
+use App\Http\Requests\StoreJoinRequestRequest;
+use App\Http\Requests\UpdateJoinRequestRequest;
+
+class JoinRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJoinRequestRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JoinRequest $joinRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JoinRequest $joinRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJoinRequestRequest $request, JoinRequest $joinRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JoinRequest $joinRequest)
+    {
+        //
+    }
+}
