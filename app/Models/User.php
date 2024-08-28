500f29f6e363d5013007040302e8eed687fe7616
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 01:01:09 +0300

    Added casts field in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,4 +16,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
