d11772039fdd4e810c887b5264fd44ec9d9f1909
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 00:59:20 +0300

    Added fillable field in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'type', 'date_of_birth', 'address', 'profile_picture', 'parent_id', 'parent_name', 'parent_email',
+    ];
 }
