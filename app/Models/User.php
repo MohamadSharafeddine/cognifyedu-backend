fdfa8871093ba045654184e696786e525eaaa983
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 01:08:20 +0300

    Added spacing between functions for better readability

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,22 +11,37 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     protected $fillable = [
-        'name', 'email', 'password', 'type', 'date_of_birth', 'address', 'profile_picture', 'parent_id', 'parent_name', 'parent_email',
+        'name',
+        'email',
+        'password',
+        'type',
+        'date_of_birth',
+        'address',
+        'profile_picture',
+        'parent_id',
+        'parent_name',
+        'parent_email',
     ];
+
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
     public function courses()
     {
         return $this->hasMany(Course::class, 'teacher_id');
     }
+
     public function parent()
     {
         return $this->belongsTo(User::class, 'parent_id');
     }
+
     public function students()
     {
         return $this->hasMany(User::class, 'parent_id');
