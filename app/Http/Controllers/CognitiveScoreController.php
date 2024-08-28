646aeae61fcab39c6708b2583f84192d33d84cb1
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-28 23:48:58 +0300

    Created CognitiveScore model and its associated files

---
+++ app/Http/Controllers/CognitiveScoreController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CognitiveScore;
+use App\Http\Requests\StoreCognitiveScoreRequest;
+use App\Http\Requests\UpdateCognitiveScoreRequest;
+
+class CognitiveScoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCognitiveScoreRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CognitiveScore $cognitiveScore)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CognitiveScore $cognitiveScore)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCognitiveScoreRequest $request, CognitiveScore $cognitiveScore)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CognitiveScore $cognitiveScore)
+    {
+        //
+    }
+}
---
+++ app/Models/CognitiveScore.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CognitiveScore extends Model
+{
+    use HasFactory;
+}
