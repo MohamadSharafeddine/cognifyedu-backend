578009e72b11d619eb921da2957598d735163bbc
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 01:42:14 +0300

    Filled the User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,22 +2,29 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = User::class;
+
+    public function definition()
     {
+        $type = $this->faker->randomElement(['teacher', 'student', 'admin']);
+        $isStudent = $type === 'student';
+    
         return [
-            //
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10),
+            'type' => $type,
+            'date_of_birth' => $this->faker->date(),
+            'address' => $this->faker->address(),
+            'parent_id' => $isStudent ? null : $this->faker->optional()->randomElement(User::where('type', 'student')->pluck('id')->toArray()),
         ];
-    }
+    }  
 }
