e0a757242e7a3cf351afaa6f54ad50ddca033fcb
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-28 23:49:33 +0300

    Created BehavioralScore model and its associated files

---
+++ app/Http/Controllers/BehavioralScoreController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BehavioralScore;
+use App\Http\Requests\StoreBehavioralScoreRequest;
+use App\Http\Requests\UpdateBehavioralScoreRequest;
+
+class BehavioralScoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBehavioralScoreRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BehavioralScore $behavioralScore)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BehavioralScore $behavioralScore)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBehavioralScoreRequest $request, BehavioralScore $behavioralScore)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BehavioralScore $behavioralScore)
+    {
+        //
+    }
+}
---
+++ database/seeders/BehavioralScoreSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BehavioralScoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
