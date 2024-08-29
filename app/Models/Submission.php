9f10cb83cc2dadfa3f0b66b15f420b92f6c31e42
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 21:45:20 +0300

    Added fillable field in Submission model

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Submission extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'assignment_id',
+        'student_id',
+        'deliverable',
+        'submission_date',
+        'mark',
+        'teacher_comment',
+    ];
+
 }
