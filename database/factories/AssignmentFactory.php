9b6f65ec8a2f5c43e4bea89427d6f1eaff6b3aa9
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-30 02:15:21 +0300

    Filled the Assignment factory

--- database/factories/AssignmentFactory.php
+++ database/factories/AssignmentFactory.php
@@ -2,13 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\Assignment;
+use App\Models\Course;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Assignment>
- */
 class AssignmentFactory extends Factory
 {
+    protected $model = Assignment::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +18,11 @@ class AssignmentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'course_id' => Course::inRandomOrder()->first()->id,
+            'title' => $this->faker->sentence(4), 
+            'description' => $this->faker->paragraph(), 
+            'attachment' => $this->faker->optional()->url(),
+            'due_date' => $this->faker->dateTimeBetween('now', '+1 month'),
         ];
     }
 }
