88c5e25e4eda3bc399d3df8bb4b021e208a87c38
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 17:32:24 +0300

    Added assignment_id foreign key to cognitive_scores table

--- database/migrations/2024_08_28_204839_create_cognitive_scores_table.php
+++ database/migrations/2024_08_28_204839_create_cognitive_scores_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('cognitive_scores', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('assignment_id')->constrained('assignments')->onDelete('cascade');
             $table->timestamps();
         });
     }
