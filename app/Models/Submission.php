4f75fdbcf5093c26e41b4acac67b43aaf9a157e2
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 21:46:04 +0300

    Added student public function in Submission model

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -23,4 +23,8 @@ class Submission extends Model
         return $this->belongsTo(Assignment::class);
     }
 
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
 }
