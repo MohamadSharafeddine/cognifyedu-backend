ee60d0139eb95bbd8d90b528d69d08d22f299169
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-30 02:38:05 +0300

    Filled ProfileComment seeder

--- database/seeders/ProfileCommentSeeder.php
+++ database/seeders/ProfileCommentSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\ProfileComment;
 
 class ProfileCommentSeeder extends Seeder
 {
@@ -12,6 +12,6 @@ class ProfileCommentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        ProfileComment::factory()->count(30)->create();
     }
 }
