9f3a646e041532cbf371afeddf21cfa4d247abbb
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-30 02:24:30 +0300

    Filled the CognitiveScore factory

--- database/factories/CognitiveScoreFactory.php
+++ database/factories/CognitiveScoreFactory.php
@@ -2,13 +2,15 @@
 
 namespace Database\Factories;
 
+use App\Models\CognitiveScore;
+use App\Models\Assignment;
+use App\Models\Submission;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CognitiveScore>
- */
 class CognitiveScoreFactory extends Factory
 {
+    protected $model = CognitiveScore::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +19,13 @@ class CognitiveScoreFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'assignment_id' => Assignment::inRandomOrder()->first()->id,
+            'submission_id' => Submission::inRandomOrder()->first()->id,
+            'critical_thinking' => $this->faker->numberBetween(1, 100),
+            'logical_thinking' => $this->faker->numberBetween(1, 100),
+            'linguistic_ability' => $this->faker->numberBetween(1, 100),
+            'memory' => $this->faker->numberBetween(1, 100),
+            'attention_to_detail' => $this->faker->numberBetween(1, 100),
         ];
     }
 }
