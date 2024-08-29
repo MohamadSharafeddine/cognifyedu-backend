b95403103420720c06811f63a66c8c022c1af943
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-30 02:25:45 +0300

    Filled the BehavioralScore factory

--- database/factories/BehavioralScoreFactory.php
+++ database/factories/BehavioralScoreFactory.php
@@ -2,13 +2,15 @@
 
 namespace Database\Factories;
 
+use App\Models\BehavioralScore;
+use App\Models\Assignment;
+use App\Models\Submission;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BehavioralScore>
- */
 class BehavioralScoreFactory extends Factory
 {
+    protected $model = BehavioralScore::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +19,13 @@ class BehavioralScoreFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'assignment_id' => Assignment::inRandomOrder()->first()->id,
+            'submission_id' => Submission::inRandomOrder()->first()->id,
+            'engagement' => $this->faker->numberBetween(1, 100),
+            'time_management' => $this->faker->numberBetween(1, 100),
+            'adaptability' => $this->faker->numberBetween(1, 100),
+            'collaboration' => $this->faker->numberBetween(1, 100),
+            'focus' => $this->faker->numberBetween(1, 100),
         ];
     }
 }
