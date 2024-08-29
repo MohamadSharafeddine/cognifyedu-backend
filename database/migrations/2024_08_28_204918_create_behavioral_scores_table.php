6fa2b855463e2f799f6ffb1af0e5831554f5dcca
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 17:45:16 +0300

    Added focus column to behavioral_scores table

--- database/migrations/2024_08_28_204918_create_behavioral_scores_table.php
+++ database/migrations/2024_08_28_204918_create_behavioral_scores_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('time_management');
             $table->integer('adaptability');
             $table->integer('collaboration');
+            $table->integer('focus');
             $table->timestamps();
         });
     }
