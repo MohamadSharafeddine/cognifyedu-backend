06b9653fc750b56a22b84079ea3575336d447ec3
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 18:26:04 +0300

    Added student_id foreign key to insights table

--- database/migrations/2024_08_28_204949_create_insights_table.php
+++ database/migrations/2024_08_28_204949_create_insights_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('insights', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
