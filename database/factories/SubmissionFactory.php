5e05545749f45eeba3d8cda96a0d02fe60e9f21a
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-30 02:21:36 +0300

    Filled the Submission factory

--- database/factories/SubmissionFactory.php
+++ database/factories/SubmissionFactory.php
@@ -2,13 +2,15 @@
 
 namespace Database\Factories;
 
+use App\Models\Submission;
+use App\Models\Assignment;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Submission>
- */
 class SubmissionFactory extends Factory
 {
+    protected $model = Submission::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +19,12 @@ class SubmissionFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'assignment_id' => Assignment::inRandomOrder()->first()->id, 
+            'student_id' => User::where('type', 'student')->inRandomOrder()->first()->id,
+            'deliverable' => $this->faker->url(),
+            'submission_date' => $this->faker->dateTimeBetween('-1 week', 'now'),
+            'mark' => $this->faker->numberBetween(0, 100),
+            'teacher_comment' => $this->faker->optional()->sentence(),
         ];
     }
 }
