829c473be0455ec6070ffee568982344a2de4054
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 18:26:33 +0300

    Added cognitive_score_id foreign key to insights table

--- database/migrations/2024_08_28_204949_create_insights_table.php
+++ database/migrations/2024_08_28_204949_create_insights_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('insights', function (Blueprint $table) {
             $table->id();
             $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('cognitive_score_id')->constrained('cognitive_scores')->onDelete('cascade');
             $table->timestamps();
         });
     }
