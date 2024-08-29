f3b82a6a21572e35a3a385820a951484f3b651de
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 18:36:46 +0300

    Added teacher_id foreign key to profile_comments table

--- database/migrations/2024_08_28_210613_create_profile_comments_table.php
+++ database/migrations/2024_08_28_210613_create_profile_comments_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('profile_comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('teacher_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
