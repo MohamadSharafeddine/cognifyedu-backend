7354d2e704829d6cb9ec431d3a23483721b0b455
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 17:44:48 +0300

    Added collaboration column to behavioral_scores table

--- database/migrations/2024_08_28_204918_create_behavioral_scores_table.php
+++ database/migrations/2024_08_28_204918_create_behavioral_scores_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->integer('engagement');
             $table->integer('time_management');
             $table->integer('adaptability');
+            $table->integer('collaboration');
             $table->timestamps();
         });
     }
