b5e06a24fdf975f4c78d9fb6527fe8648f14d283
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-30 02:39:16 +0300

    Filled the Insight factory

--- database/factories/InsightFactory.php
+++ database/factories/InsightFactory.php
@@ -2,13 +2,17 @@
 
 namespace Database\Factories;
 
+use App\Models\Insight;
+use App\Models\CognitiveScore;
+use App\Models\BehavioralScore;
+use App\Models\ProfileComment;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Insight>
- */
 class InsightFactory extends Factory
 {
+    protected $model = Insight::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,14 @@ class InsightFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'student_id' => User::where('type', 'student')->inRandomOrder()->first()->id, 
+            'cognitive_score_id' => CognitiveScore::inRandomOrder()->first()->id, 
+            'behavioral_score_id' => BehavioralScore::inRandomOrder()->first()->id,
+            'profile_comment_id' => ProfileComment::inRandomOrder()->first()->id,
+            'summary' => $this->faker->paragraph(), 
+            'detailed_analysis' => $this->faker->text(),
+            'recommendations' => $this->faker->text(),
+            'progress_tracking' => $this->faker->text(),
         ];
     }
 }
