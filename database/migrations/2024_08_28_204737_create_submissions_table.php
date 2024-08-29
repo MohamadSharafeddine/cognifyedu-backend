1ba6c73c46fef6eb71c7652c86c57aea47e6b782
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 21:38:49 +0300

    Added assignment_id foregin key to submissions table

--- database/migrations/2024_08_28_204737_create_submissions_table.php
+++ database/migrations/2024_08_28_204737_create_submissions_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('submissions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('assignment_id')->constrained('assignments')->onDelete('cascade');
             $table->timestamps();
         });
     }
