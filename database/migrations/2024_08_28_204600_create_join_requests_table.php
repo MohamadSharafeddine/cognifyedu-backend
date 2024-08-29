90116ec55559a716a50b140ae6ec3221db44e63d
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-29 21:49:09 +0300

    Added course_id foreign key to join_requests table

--- database/migrations/2024_08_28_204600_create_join_requests_table.php
+++ database/migrations/2024_08_28_204600_create_join_requests_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('join_requests', function (Blueprint $table) {
             $table->id();
             $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
             $table->timestamps();
         });
     }
