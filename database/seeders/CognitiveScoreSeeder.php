abf0bfd297f6068dec384f07463456089d2d8ad0
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-30 02:24:57 +0300

    Filled the CognitiveScore seeder

--- database/seeders/CognitiveScoreSeeder.php
+++ database/seeders/CognitiveScoreSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\CognitiveScore;
 
 class CognitiveScoreSeeder extends Seeder
 {
@@ -12,6 +12,6 @@ class CognitiveScoreSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        CognitiveScore::factory()->count(50)->create();
     }
 }
