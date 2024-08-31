b4092acbf6a6bbdb665deea7514501a4f26ab9ed
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-01 02:02:36 +0300

    Filled the rules public function in StoreUserRequest to require name email password and user type to register

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -22,7 +22,14 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'type' => 'required|in:teacher,student,parent,admin',
+            'date_of_birth' => 'nullable|date',
+            'address' => 'nullable|string|max:255',
+            'profile_picture' => 'nullable|string|max:255',
+            'parent_id' => 'nullable|exists:users,id',
         ];
-    }
+    }    
 }
