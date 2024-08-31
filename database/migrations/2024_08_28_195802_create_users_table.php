3b12c36e58863ddcb2b5dc39fe03dd09c4b81386
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-01 01:59:40 +0300

    Made date of birth nullable in users table

--- database/migrations/2024_08_28_195802_create_users_table.php
+++ database/migrations/2024_08_28_195802_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('type', ['teacher', 'student', 'parent', 'admin']);
-            $table->date('date_of_birth');
+            $table->date('date_of_birth')->nullable();
             $table->string('address')->nullable();
             $table->string('profile_picture')->nullable();
             $table->foreignId('parent_id')->nullable()->constrained('users')->onDelete('set null');
