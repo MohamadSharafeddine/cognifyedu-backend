e29ce0f302076f94939ece86f88756f41e5e2a58
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-02 15:21:27 +0300

    Inserted SubmissionController into api to add routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\AssignmentController;
+use App\Http\Controllers\SubmissionController;
 
 /*
 |--------------------------------------------------------------------------
