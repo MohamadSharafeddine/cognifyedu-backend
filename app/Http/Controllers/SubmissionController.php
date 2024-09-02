a467ccf088eeb3520cdfe9fa4810b3c4e0358dd8
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-02 17:23:16 +0300

    Filled the show function in SubmissionController

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -48,7 +48,7 @@ class SubmissionController extends Controller
      */
     public function show(Submission $submission)
     {
-        //
+        return response()->json($submission);
     }
 
     /**
