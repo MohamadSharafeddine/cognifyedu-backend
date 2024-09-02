ac7db0b82e917ec928bc936e4434551a9c6fb18e
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-02 21:58:58 +0300

    Filled CoureStudent seeder

--- database/seeders/CourseStudentSeeder.php
+++ database/seeders/CourseStudentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\CourseStudent;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class CourseStudentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        CourseStudent::factory()->count(10)->create();
     }
 }
