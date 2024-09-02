4a3d936c4ef7099f23482f2bd7c706dd97ec306b
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-02 14:06:43 +0300

    Inserted CourseController into api to add routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,4 @@ Route::group([
     Route::delete('/{user}', 'destroy');
     Route::post('/logout',  'logout');
 });
+
