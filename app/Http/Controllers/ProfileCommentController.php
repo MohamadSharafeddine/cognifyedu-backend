29803e9d403cdee32cb1d49b90b5d32236dd2b3b
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-02 23:58:32 +0300

    Filled the show function in ProfileCommentControllers

--- app/Http/Controllers/ProfileCommentController.php
+++ app/Http/Controllers/ProfileCommentController.php
@@ -39,7 +39,7 @@ class ProfileCommentController extends Controller
      */
     public function show(ProfileComment $profileComment)
     {
-        //
+        return response()->json($profileComment);
     }
 
     /**
