7a1c2487511416ea366609763d7336185c914916
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-02 21:59:36 +0300

    Added CourseStudentSeeder to DataBaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CourseSeeder::class,
+            CourseStudentSeeder::class,
             AssignmentSeeder::class,
             SubmissionSeeder::class,
             CognitiveScoreSeeder::class,
