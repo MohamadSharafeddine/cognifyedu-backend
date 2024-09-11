fb519cd56b3ef44ecf5db737a964a3d4ed5c4e96
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-11 17:59:10 +0300

    Added routes for new functions, cleaned code, and rearranged routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CourseController;
@@ -26,109 +25,81 @@ use App\Http\Controllers\InsightController;
 Route::post('/register', [UserController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'users',
-    'controller' => UserController::class], function () {
-    Route::get('/', 'index');
-    Route::get('/{user}', 'show');
-    Route::post('/{user}','update');
-    Route::delete('/{user}', 'destroy');
-    Route::post('/logout',  'logout');
-});
+Route::middleware('auth:api')->group(function () {
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'courses',
-    'controller' => CourseController::class
-], function () {
-    Route::get('/', 'index');
-    Route::post('/', 'store');
-    Route::get('/{course}', 'show');
-    Route::post('/{course}', 'update');
-    Route::delete('/{course}', 'destroy');
-});
+    Route::prefix('users')->controller(UserController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/{user}', 'show');
+        Route::post('/{user}', 'update');
+        Route::delete('/{user}', 'destroy');
+        Route::post('/logout', 'logout');
+    });
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'assignments',
-    'controller' => AssignmentController::class
-], function () {
-    Route::get('/', 'index');
-    Route::post('/', 'store');
-    Route::get('/{assignment}', 'show');
-    Route::post('/{assignment}', 'update');
-    Route::delete('/{assignment}', 'destroy');
-});
+    Route::prefix('courses')->controller(CourseController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{course}', 'show');
+        Route::post('/{course}', 'update');
+        Route::delete('/{course}', 'destroy');
+        Route::get('/{courseId}/students', 'getStudents');
+        Route::get('/user/{userId}', 'getCoursesByUserId');
+    });
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'submissions',
-    'controller' => SubmissionController::class
-], function () {
-    Route::get('/', 'index');
-    Route::post('/', 'store');
-    Route::get('/{submission}', 'show');
-    Route::post('/{submission}', 'update');
-    Route::delete('/{submission}', 'destroy');
-});
+    Route::prefix('assignments')->controller(AssignmentController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{assignment}', 'show');
+        Route::post('/{assignment}', 'update');
+        Route::delete('/{assignment}', 'destroy');
+        Route::get('/class/{classId}', 'getAssignmentsForClass');
+    });
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'course-students',
-    'controller' => CourseStudentController::class
-], function () {
-    Route::get('/', 'index');
-    Route::post('/', 'store');
-    Route::get('/{courseStudent}', 'show');
-    Route::post('/{courseStudent}', 'update');
-    Route::delete('/{courseStudent}', 'destroy');
-});
+    Route::prefix('submissions')->controller(SubmissionController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{submission}', 'show');
+        Route::post('/{submission}', 'update');
+        Route::delete('/{submission}', 'destroy');
+        Route::get('/assignment/{assignmentId}', 'getSubmissionsForAssignment');
+    });
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'profile-comments',
-    'controller' => ProfileCommentController::class
-], function () {
-    Route::get('/', 'index');
-    Route::post('/', 'store');
-    Route::get('/{profileComment}', 'show');
-    Route::post('/{profileComment}', 'update');
-    Route::delete('/{profileComment}', 'destroy');
-});
+    Route::prefix('course-students')->controller(CourseStudentController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{courseStudent}', 'show');
+        Route::post('/{courseStudent}', 'update');
+        Route::delete('/{courseStudent}', 'destroy');
+    });
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'cognitive-scores',
-    'controller' => CognitiveScoreController::class
-], function () {
-    Route::get('/', 'index');
-    Route::post('/', 'store');
-    Route::get('/{cognitiveScore}', 'show');
-    Route::post('/{cognitiveScore}', 'update');
-    Route::delete('/{cognitiveScore}', 'destroy');
-});
+    Route::prefix('profile-comments')->controller(ProfileCommentController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{profileComment}', 'show');
+        Route::post('/{profileComment}', 'update');
+        Route::delete('/{profileComment}', 'destroy');
+    });
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'behavioral-scores',
-    'controller' => BehavioralScoreController::class
-], function () {
-    Route::get('/', 'index');
-    Route::post('/', 'store');
-    Route::get('/{behavioralScore}', 'show');
-    Route::post('/{behavioralScore}', 'update');
-    Route::delete('/{behavioralScore}', 'destroy');
-});
+    Route::prefix('cognitive-scores')->controller(CognitiveScoreController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{cognitiveScore}', 'show');
+        Route::post('/{cognitiveScore}', 'update');
+        Route::delete('/{cognitiveScore}', 'destroy');
+    });
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'insights',
-    'controller' => InsightController::class
-], function () {
-    Route::get('/', 'index');
-    Route::post('/', 'store');
-    Route::get('/{insight}', 'show');
-    Route::post('/{insight}', 'update');
-    Route::delete('/{insight}', 'destroy');
+    Route::prefix('behavioral-scores')->controller(BehavioralScoreController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{behavioralScore}', 'show');
+        Route::post('/{behavioralScore}', 'update');
+        Route::delete('/{behavioralScore}', 'destroy');
+    });
+
+    Route::prefix('insights')->controller(InsightController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{insight}', 'show');
+        Route::post('/{insight}', 'update');
+        Route::delete('/{insight}', 'destroy');
+    });
 });
