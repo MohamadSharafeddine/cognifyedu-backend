2b19b7a9feee0ef6e85cb29ec145d23e03dfee39
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-13 14:47:00 +0300

    Removed submission date from rules since it now gets it from the backend

--- app/Http/Requests/UpdateSubmissionRequest.php
+++ app/Http/Requests/UpdateSubmissionRequest.php
@@ -6,26 +6,16 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateSubmissionRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'assignment_id' => 'sometimes|exists:assignments,id',
-            'student_id' => 'sometimes|exists:users,id',
-            'deliverable' => 'sometimes|file|mimes:txt|max:10240',
-            'submission_date' => 'sometimes|date',
+            'deliverable' => 'required|file|mimes:txt,pdf,doc,docx|max:10240', // Ensure the file size is correct
             'mark' => 'nullable|integer|min:0|max:100',
             'teacher_comment' => 'nullable|string',
         ];
