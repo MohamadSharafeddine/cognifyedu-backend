385cea620e1617949a2dbc59b75d7ffe5df555a7
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-13 14:48:25 +0300

    Added support for different types of doc files

--- app/Http/Requests/StoreAssignmentRequest.php
+++ app/Http/Requests/StoreAssignmentRequest.php
@@ -6,26 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreAssignmentRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'course_id' => 'required|exists:courses,id',
             'title' => 'required|string|max:255',
             'description' => 'nullable|string',
-            'attachment' => 'sometimes|file|mimes:txt|max:10240',
+            'attachment' => 'sometimes|file|mimes:txt,pdf,doc,docx|max:10240',
             'due_date' => 'required|date',
         ];
     }
--- app/Http/Requests/UpdateAssignmentRequest.php
+++ app/Http/Requests/UpdateAssignmentRequest.php
@@ -6,26 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateAssignmentRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'course_id' => 'sometimes|exists:courses,id',
             'title' => 'sometimes|string|max:255',
             'description' => 'nullable|string',
-            'attachment' => 'sometimes|file|mimes:txt|max:10240',
+            'attachment' => 'sometimes|file|mimes:txt,pdf,doc,docx|max:10240', // Optional file update
             'due_date' => 'sometimes|date',
         ];
     }
